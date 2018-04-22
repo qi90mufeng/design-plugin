19d31c70a4ba77db5f41f59896065f4e18242529
qi90mufeng <qimufeng1990@163.com>
2018-04-22 12:21:22 +0800

    参数传值  注释
    pom增加utf-8，规定不会乱码

--- src/main/java/mufeng/MfMojo.java
+++ src/main/java/mufeng/MfMojo.java
@@ -5,13 +5,21 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 /**
- * @goal descriptor
+ * @goal introduce
  */
-@Mojo(name ="mufeng", defaultPhase = LifecyclePhase.PACKAGE)
+@Mojo(name ="introduce", defaultPhase = LifecyclePhase.PACKAGE)
 public class MfMojo extends AbstractMojo{
+
+    /**
+     * @parameter expression="${msg}"
+     * @required
+     */
+    private String msg;
+
     public void execute() throws MojoExecutionException, MojoFailureException {
-        System.out.println("神保佑你 插件");
+        System.out.println("神保佑你 插件:" + msg);
     }
 }
